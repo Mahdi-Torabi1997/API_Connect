9af503fdc3d4aede86f88b9649b70f16ed76c18b
Mahdi-Torabi1997 <Mato.1376@gmail.com>
2024-07-03 14:17:08 -0700

    fourth commit

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ import requests
 import pandas as pd
 
 # Constants for API authentication
-CLIENT_ID = "lCbN00xtQ5OC8tZo"
-CLIENT_SECRET = "AgV30faxOYr4ozl1p8m97uaamw0gQgZNadowlTjbCOqOG4Ew5TEz6G8JSu1EdFfM"
+CLIENT_ID = "NnPOAzszR8TKZVnT"
+CLIENT_SECRET = "lvvdFj8p58YRTjWQACMzDo4PIopuKfNgxkhXr1nehblkQpRDQSNRgoDzk2C2BBCx"
 
 ################# Utility Functions ###################
 def parseStringInt32(stringData, startIndex):
